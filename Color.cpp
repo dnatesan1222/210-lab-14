7956cf94b74321441d3e069532c271d396fd8df8
Diksha <diksha@macbookpro>
2025-09-26 16:24:18 -0700

    started color class

---
+++ Color.cpp
@@ -0,0 +1,16 @@
+// COMSC-210 | Lab 14 | Diksha Tara Natesan
+// IDE used: Vim (Terminal)
+
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+class Color{
+private:
+    red;
+    green;
+    blue;
+
+};
